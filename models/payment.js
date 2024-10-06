0539aab1a11f65050111b86eb55147a55ab49b91
alibha04 <alibha.pra@gmail.com>
2024-10-06 17:52:37 +0000

    full stack

--- models/payment.js
+++ models/payment.js
@@ -2,11 +2,7 @@ const mongoose =require ("mongoose");
 const {Schema} = mongoose;
 
 const paymentSchema = new Schema({
-    reservation:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Reservation',
-        required: true
-    },
+    
     paymentAmount:{
         type: Number,
         required: true
