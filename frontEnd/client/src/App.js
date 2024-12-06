71fe620876eac523c6149860df644b2357f23fc8
alibha04 <alibha.pra@gmail.com>
2024-12-06 17:04:37 +0000

    frontend updated

--- frontEnd/client/src/App.js
+++ frontEnd/client/src/App.js
@@ -4,8 +4,8 @@ import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, Box } from '@mui/material';
 import solarizedDarkTheme from './theme/solarizedDarkTheme';
 
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Navbar';
+import { Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 
@@ -22,14 +22,14 @@ const App = () => {
       <Box display="flex" flexDirection="column" minHeight="100vh">
         <Navbar />
         <Box component="main" flexGrow={1} py={3}>
-          <Routes>
+          {/* <Routes>
             <Route exact path='/' element={<HomePage />} />
             <Route path='/book-list' element={<ShowBookList />} />
             <Route path='/create-book' element={<CreateBook />} />
             <Route path='/edit-book/:id' element={<UpdateBookInfo />} />
             <Route path='/show-book/:id' element={<ShowBookDetails />} />
             <Route path='/notes/*' element={<NotesPage />} />
-          </Routes>
+          </Routes> */}
         </Box>
         <Footer />
       </Box>
--- frontEnd/client/src/pages/Home.js
+++ frontEnd/client/src/pages/Home.js
@@ -3,18 +3,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Navbar = () => {
   return (
     <header>
       <h1>Parking System</h1>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/parking-lots">Parking Lots</Link>
+        {/* <Link to="/parking-lots">Parking Lots</Link>
         <Link to="/reservations">Reservations</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link>  */}
       </nav>
     </header>
   );
 };
 
-export default Header;
+export default Navbar;
+
