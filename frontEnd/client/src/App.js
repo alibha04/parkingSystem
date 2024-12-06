1232dd3f0606b80032004798c52beb1858217780
alibha04 <alibha.pra@gmail.com>
2024-12-06 16:50:03 +0000

    frontend updated

--- frontEnd/client/src/App.js
+++ frontEnd/client/src/App.js
@@ -1,7 +1,11 @@
 // src/App.js
 import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Box } from '@mui/material';
+import solarizedDarkTheme from './theme/solarizedDarkTheme';
+
 import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 
@@ -12,15 +16,26 @@ import Home from './pages/Home';
 
 const App = () => {
   return (
-    <BrowserRouter >
-      <Header />
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/parking-lots" element={<ParkingLots />} /> */}
-        {/* <Route path="/reservations" element={<Reservations />} /> */}
-        {/* <Route path="/login" element={<User />} /> */}
-      <Footer />
-    </BrowserRouter>
-  );
+    <ThemeProvider theme={solarizedDarkTheme}>
+    <CssBaseline />
+    <Router>
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <Navbar />
+        <Box component="main" flexGrow={1} py={3}>
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route path='/book-list' element={<ShowBookList />} />
+            <Route path='/create-book' element={<CreateBook />} />
+            <Route path='/edit-book/:id' element={<UpdateBookInfo />} />
+            <Route path='/show-book/:id' element={<ShowBookDetails />} />
+            <Route path='/notes/*' element={<NotesPage />} />
+          </Routes>
+        </Box>
+        <Footer />
+      </Box>
+    </Router>
+  </ThemeProvider>
+);
 };
 
 export default App;
--- frontEnd/client/src/components/HomePage.js
+++ frontEnd/client/src/components/HomePage.js
@@ -7,15 +7,15 @@ const HomePage = () => {
   return (
     <Container maxWidth="lg" sx={{ textAlign: 'center', py: 5 }}>
       <Typography variant="h2" component="h1" color="primary" gutterBottom>
-        Welcome to the Book Management System
+        Welcome to the Parking Lot System
       </Typography>
       <Typography variant="h5" gutterBottom>
-        Manage your books efficiently and effectively.
+        Park your vehicles with out any chaos
       </Typography>
       <Box mt={4}>
         <Button 
           component={Link} 
-          to="/book-list" // Updated to link to the ShowBookList component
+          to="/parkingLotList" // Updated to link to the ShowBookList component
           color="primary" 
           variant="contained"
         >
