df17345d5aa0b5d751cc180ac9051fc8595d4f9e
alibha04 <alibha.pra@gmail.com>
2025-01-04 15:35:33 +0000

    updated navbar

--- frontEnd/client/src/components/Navbar.js
+++ frontEnd/client/src/components/Navbar.js
@@ -1,106 +1,18 @@
-// import React, { useState } from 'react';
-// import { Link as RouterLink } from 'react-router-dom';
-// import {
-//   AppBar,
-//   Toolbar,
-//   Typography,
-//   Button,
-//   Box,
-//   IconButton,
-//   Menu,
-//   MenuItem,
-// } from '@mui/material';
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import HomeIcon from '@mui/icons-material/Home';
-// import MenuBookIcon from '@mui/icons-material/MenuBook'; // Added Notes icon
-
-// const notesPages = [
-//   { title: 'Home', path: '/notes/home' },
-//   { title: 'Schedule', path: '/notes/schedule' },
-// ];
-
-// const Navbar = () => {
-//   const [notesAnchorEl, setNotesAnchorEl] = useState(null);
-
-//   const handleNotesClick = (event) => {
-//     setNotesAnchorEl(event.currentTarget);
-//   };
-
-//   const handleNotesClose = () => {
-//     setNotesAnchorEl(null);
-//   };
-
-//   return (
-//     <AppBar position="static" color="transparent" elevation={0} sx={{ width: '100%' }}>
-//       <Toolbar>
-//         <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'primary.main' }}>
-//           Parking System 
-//         </Typography>
-//         <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-//           <Button
-//             color="primary"
-//             component={RouterLink}
-//             to="/"
-//             startIcon={<HomeIcon />}
-//           >
-//             Home
-//           </Button>
-//           <Button
-//             color="primary"
-//             onClick={handleNotesClick}
-//             startIcon={<MenuBookIcon />} // Added icon here
-//           >
-//             Details
-//           </Button>
-//           <Menu
-//             anchorEl={notesAnchorEl}
-//             open={Boolean(notesAnchorEl)}
-//             onClose={handleNotesClose}
-//           >
-//             {notesPages.map((page) => (
-//               <MenuItem 
-//                 key={page.path} 
-//                 component={RouterLink} 
-//                 to={page.path}
-//                 onClick={handleNotesClose}
-//               >
-//                 {page.title}
-//               </MenuItem>
-//             ))}
-//           </Menu>
-          // <IconButton
-          //   color="primary"
-          //   component="a"
-          //   href="https://github.com/alibha04/parkingSystem"
-          //   target="_blank"
-          //   rel="noopener noreferrer"
-          //   aria-label="GitHub"
-          // >
-          //   <GitHubIcon />
-          // </IconButton>
-//         </Box>
-//       </Toolbar>
-//     </AppBar>
-//   );
-// };
-
-// export default Navbar;
-
-
-
-
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
   Typography,
-  Button,
-  Box,
   IconButton,
+  Box,
   Menu,
   MenuItem,
+  Button,
+  Tooltip,
+
 } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
@@ -109,6 +21,9 @@ import DownloadIcon from '@mui/icons-material/Download';
 
 const Navbar = () => {
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => setMenuAnchorEl(event.currentTarget);
+
 
   const handleMenuClick = (event) => {
     setMenuAnchorEl(event.currentTarget);
@@ -166,13 +81,40 @@ const Navbar = () => {
           <IconButton
             color="primary"
             component="a"
-            href="https://docs.google.com/document/d/1951CLEB80bJ5kHb3fJa355BsURLlvO9wbkWmdBRDtbk/edit?usp=drivesdk"
+            href="https://docs.google.com/document/d/1CWyqXhAvyTfxwv0Giqa-ITFZZYGYbootp2S3_bSeOf8/edit?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Resume"
           >
             <MenuBookIcon />
           </IconButton>
+
+
+          <IconButton onClick={handleMenuOpen} color="inherit" position="left" >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    anchorEl={menuAnchorEl}
+                    open={Boolean(menuAnchorEl)}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem component={RouterLink} to="/add" onClick={handleMenuClose}>
+                        Create Lot
+                    </MenuItem>
+                    <MenuItem component={RouterLink} to="/list" onClick={handleMenuClose}>
+                        Lot List
+                    </MenuItem>
+                    <MenuItem component={RouterLink} to="/search" onClick={handleMenuClose}>
+                        Search Lot
+                    </MenuItem>
+                    <MenuItem component={RouterLink} to="/export" onClick={handleMenuClose}>
+                        Download QR
+                    </MenuItem>
+                    <MenuItem component={RouterLink} to="/about" onClick={handleMenuClose}>
+                        About
+                    </MenuItem>
+                </Menu>
+
         </Box>
       </Toolbar>
     </AppBar>
